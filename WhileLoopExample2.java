70d81e6179277fcf6ca8a2b074b2a3125550933e
Backend Developer <hseyn.mmmdzad@bk.ru>
2025-09-07 13:34:18 +0400

    Add files via upload

---
+++ WhileLoopExample2.java
@@ -0,0 +1,21 @@
+package yenipabka;
+
+public class WhileLoopExample2 {
+
+    public static void main(String[] args) {
+
+
+        int geriSayim = 10;
+        System.out.println("Geri sayım başlayır:");
+        while (geriSayim >= 0) {
+            System.out.println(geriSayim);
+            geriSayim--;
+
+
+        }
+        System.out.println("Partladı!");
+
+    }
+
+
+}
